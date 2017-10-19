1f8a40a5b855796a13a7ccb2f5721dbc01dee4b8
Yuriy Volyanyuk <yzik333@outlook.com>
2017-10-19 16:14:23 +0300

    dobre dobre

--- Stomatology/Patient/NewPatient.cs
+++ Stomatology/Patient/NewPatient.cs
@@ -7,14 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Stomatology
 {
     public partial class NewPatient : Form
     {
+
+        SqlConnection testCon = new SqlConnection
+        (@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=F:\Program development\Stomatology\StomatologyData.mdf;Integrated Security=True");
+
         public NewPatient()
         {
             InitializeComponent();
+        }
+
+        private void NewPatient_Load(object sender, EventArgs e)
+        {
+            testCon.Open();
+            SqlCommand cmd = testCon.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = $"INSERT INTO Client (Адрес) values('1')";
+            cmd.ExecuteNonQuery();
+            testCon.Close();
+            //updateTable();
+            testCon.Open();
+            //string uId = clientDataGridView.Rows[clientDataGridView.Rows.Count - 1].Cells[0].Value.ToString();
+           // string query = $"delete from Client where id = {uId}";
+           // SqlCommand cmd1 = new SqlCommand(query, testCon);
+            cmd1.ExecuteNonQuery();
+            testCon.Close();
+            //updateTable();
         }
 
         private void textBox5_TextChanged(object sender, EventArgs e)
