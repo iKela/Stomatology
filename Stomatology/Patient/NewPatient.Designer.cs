b6c919454768262bf3ae019e163a5f6a647f687d
Yuriy Volyanyuk <yzik333@outlook.com>
2017-10-19 17:43:43 +0300

    qwe

--- Stomatology/Patient/NewPatient.Designer.cs
+++ Stomatology/Patient/NewPatient.Designer.cs
@@ -35,13 +35,13 @@
             this.label5 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.textBoxDate = new System.Windows.Forms.TextBox();
+            this.TextboxFatherName = new System.Windows.Forms.TextBox();
+            this.TextboxName = new System.Windows.Forms.TextBox();
+            this.TextboxLastName = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBoxAdress = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -97,6 +97,7 @@
             this.button1.TabIndex = 5;
             this.button1.Text = "Додати";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -108,41 +109,40 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // textBox1
+            // textBoxNumber
             // 
-            this.textBox1.Location = new System.Drawing.Point(156, 154);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(330, 20);
-            this.textBox1.TabIndex = 7;
+            this.textBoxNumber.Location = new System.Drawing.Point(156, 154);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(330, 20);
+            this.textBoxNumber.TabIndex = 7;
             // 
-            // textBox2
+            // textBoxDate
             // 
-            this.textBox2.Location = new System.Drawing.Point(156, 121);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(330, 20);
-            this.textBox2.TabIndex = 8;
+            this.textBoxDate.Location = new System.Drawing.Point(156, 121);
+            this.textBoxDate.Name = "textBoxDate";
+            this.textBoxDate.Size = new System.Drawing.Size(330, 20);
+            this.textBoxDate.TabIndex = 8;
             // 
-            // textBox3
+            // TextboxFatherName
             // 
-            this.textBox3.Location = new System.Drawing.Point(156, 83);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(330, 20);
-            this.textBox3.TabIndex = 9;
+            this.TextboxFatherName.Location = new System.Drawing.Point(156, 83);
+            this.TextboxFatherName.Name = "TextboxFatherName";
+            this.TextboxFatherName.Size = new System.Drawing.Size(330, 20);
+            this.TextboxFatherName.TabIndex = 9;
             // 
-            // textBox4
+            // TextboxName
             // 
-            this.textBox4.Location = new System.Drawing.Point(156, 46);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(330, 20);
-            this.textBox4.TabIndex = 10;
+            this.TextboxName.Location = new System.Drawing.Point(156, 46);
+            this.TextboxName.Name = "TextboxName";
+            this.TextboxName.Size = new System.Drawing.Size(330, 20);
+            this.TextboxName.TabIndex = 10;
             // 
-            // textBox5
+            // TextboxLastName
             // 
-            this.textBox5.Location = new System.Drawing.Point(156, 10);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(330, 20);
-            this.textBox5.TabIndex = 11;
-            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            this.TextboxLastName.Location = new System.Drawing.Point(156, 10);
+            this.TextboxLastName.Name = "TextboxLastName";
+            this.TextboxLastName.Size = new System.Drawing.Size(330, 20);
+            this.TextboxLastName.TabIndex = 11;
             // 
             // label6
             // 
@@ -153,25 +153,25 @@
             this.label6.TabIndex = 12;
             this.label6.Text = "Адреса:";
             // 
-            // textBox6
+            // textBoxAdress
             // 
-            this.textBox6.Location = new System.Drawing.Point(156, 191);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(330, 20);
-            this.textBox6.TabIndex = 13;
+            this.textBoxAdress.Location = new System.Drawing.Point(156, 191);
+            this.textBoxAdress.Name = "textBoxAdress";
+            this.textBoxAdress.Size = new System.Drawing.Size(330, 20);
+            this.textBoxAdress.TabIndex = 13;
             // 
             // NewPatient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(501, 288);
-            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBoxAdress);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TextboxLastName);
+            this.Controls.Add(this.TextboxName);
+            this.Controls.Add(this.TextboxFatherName);
+            this.Controls.Add(this.textBoxDate);
+            this.Controls.Add(this.textBoxNumber);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
@@ -183,7 +183,6 @@
             this.MinimumSize = new System.Drawing.Size(517, 327);
             this.Name = "NewPatient";
             this.Text = "Новий паціент";
-            this.Load += new System.EventHandler(this.NewPatient_Load_1);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -198,12 +197,12 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.TextBox textBoxDate;
+        private System.Windows.Forms.TextBox TextboxFatherName;
+        private System.Windows.Forms.TextBox TextboxName;
+        private System.Windows.Forms.TextBox TextboxLastName;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBoxAdress;
     }
 }
--- Stomatology/Patient/NewPatient.cs
+++ Stomatology/Patient/NewPatient.cs
@@ -24,25 +24,20 @@ namespace Stomatology
 
         private void NewPatient_Load(object sender, EventArgs e)
         {
-            testCon.Open();
-            SqlCommand cmd = testCon.CreateCommand();
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = $"INSERT INTO Client (Адрес) values('1')";
-            cmd.ExecuteNonQuery();
-            testCon.Close();
-            //updateTable();
-            testCon.Open();
-            //string uId = clientDataGridView.Rows[clientDataGridView.Rows.Count - 1].Cells[0].Value.ToString();
-           // string query = $"delete from Client where id = {uId}";
-           // SqlCommand cmd1 = new SqlCommand(query, testCon);
-            cmd1.ExecuteNonQuery();
-            testCon.Close();
-            //updateTable();
-        }
-
-        private void textBox5_TextChanged(object sender, EventArgs e)
-        {
-
+           // testCon.Open();
+           // SqlCommand cmd = testCon.CreateCommand();
+           // cmd.CommandType = CommandType.Text;
+           // cmd.CommandText = $"INSERT INTO Client (Адрес) values('1')";
+           // cmd.ExecuteNonQuery();
+           // testCon.Close();
+           // //updateTable();
+           // testCon.Open();
+           // //string uId = clientDataGridView.Rows[clientDataGridView.Rows.Count - 1].Cells[0].Value.ToString();
+           //// string query = $"delete from Client where id = {uId}";
+           //// SqlCommand cmd1 = new SqlCommand(query, testCon);
+           // cmd1.ExecuteNonQuery();
+           // testCon.Close();
+           // //updateTable();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -50,9 +45,50 @@ namespace Stomatology
             this.Close();
         }
 
-        private void NewPatient_Load_1(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (TextboxLastName.Text.Length == 0 || TextboxName.Text.Length == 0 || TextboxFatherName.Text.Length == 0 ||
+                    textBoxDate.Text.Length == 0 || textBoxNumber.Text.Length == 0 || textBoxAdress.Text.Length == 0)
+                    throw new Exception("Незаповненні дані про працівника!");
+                else
+                {
+                    testCon.Open();
 
+                    //string query = $"select * from [Posada] where Посада = N'{posadaComboBox.Text}'";
+                    //SqlCommand cmd1 = new SqlCommand(query, testCon);
+                   // SqlDataReader reader = cmd1.ExecuteReader();
+                    //string posadaId = "";
+                    //if (reader.Read())
+                    //{
+                    //    posadaId = reader["Id"].ToString();
+                    //}
+                    //else
+                    //{
+                    //    throw new Exception("Не вдалось знайти таку посаду, перевірте ще раз!");
+                    //}
+
+                    //testCon.Close();
+
+                    testCon.Open();
+                    SqlCommand cmd = testCon.CreateCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = $"INSERT INTO People (Name, Прізвище, Побатькові, Дата_народження, Паспорт_серія, Контакт, id_Posada) " +
+                        $"values (N'{namebox.Text}', N'{surnamebox.Text}', N'{lastNamebox.Text}', N'{dataBirthbox.Text}', N'{passportbox.Text}', " +
+                        $"N'{contactbox.Text}', N'{posadaId}')";
+                    cmd.ExecuteNonQuery();
+                    namebox.Text = ""; surnamebox.Text = ""; lastNamebox.Text = "";
+                    passportbox.Text = ""; contactbox.Text = ""; dataBirthbox.Text = "";
+                    testCon.Close();
+                    updateTable();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Помилка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                testCon.Close();
+            }
         }
     }
 }
