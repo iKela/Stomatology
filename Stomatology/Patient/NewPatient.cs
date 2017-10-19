4a1aa9974ce312ea28d157ecf1b3683d81a62e99
Yuriy Volyanyuk <yzik333@outlook.com>
2017-10-19 17:54:18 +0300

    qwe

--- Stomatology/Patient/NewPatient.cs
+++ Stomatology/Patient/NewPatient.cs
@@ -15,7 +15,7 @@ namespace Stomatology
     {
 
         SqlConnection testCon = new SqlConnection
-        (@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=F:\Program development\Stomatology\StomatologyData.mdf;Integrated Security=True");
+        (@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\StomatologyData.mdf;Integrated Security=True");
 
         public NewPatient()
         {
@@ -24,20 +24,7 @@ namespace Stomatology
 
         private void NewPatient_Load(object sender, EventArgs e)
         {
-           // testCon.Open();
-           // SqlCommand cmd = testCon.CreateCommand();
-           // cmd.CommandType = CommandType.Text;
-           // cmd.CommandText = $"INSERT INTO Client (Адрес) values('1')";
-           // cmd.ExecuteNonQuery();
-           // testCon.Close();
-           // //updateTable();
-           // testCon.Open();
-           // //string uId = clientDataGridView.Rows[clientDataGridView.Rows.Count - 1].Cells[0].Value.ToString();
-           //// string query = $"delete from Client where id = {uId}";
-           //// SqlCommand cmd1 = new SqlCommand(query, testCon);
-           // cmd1.ExecuteNonQuery();
-           // testCon.Close();
-           // //updateTable();
+          
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -54,27 +41,11 @@ namespace Stomatology
                     throw new Exception("Незаповненні дані про працівника!");
                 else
                 {
-                    testCon.Open();
-
-                    //string query = $"select * from [Posada] where Посада = N'{posadaComboBox.Text}'";
-                    //SqlCommand cmd1 = new SqlCommand(query, testCon);
-                   // SqlDataReader reader = cmd1.ExecuteReader();
-                    //string posadaId = "";
-                    //if (reader.Read())
-                    //{
-                    //    posadaId = reader["Id"].ToString();
-                    //}
-                    //else
-                    //{
-                    //    throw new Exception("Не вдалось знайти таку посаду, перевірте ще раз!");
-                    //}
-
-                    //testCon.Close();
-
+                    
                     testCon.Open();
                     SqlCommand cmd = testCon.CreateCommand();
                     cmd.CommandType = CommandType.Text;
-                    cmd.CommandText = $"INSERT INTO People (Name, Прізвище, Побатькові, Дата_народження, Паспорт_серія, Контакт, id_Posada) " +
+                    cmd.CommandText = $"INSERT INTO Pacient (Surname, Name, FatherName, Birthday, Number, Adress) " +
                         $"values (N'{namebox.Text}', N'{surnamebox.Text}', N'{lastNamebox.Text}', N'{dataBirthbox.Text}', N'{passportbox.Text}', " +
                         $"N'{contactbox.Text}', N'{posadaId}')";
                     cmd.ExecuteNonQuery();
