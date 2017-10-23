1d86f272caa1183f660ff36963289c13f097e26c
iKela <kovalchuknm1997@gmail.com>
2017-10-23 10:25:46 +0300

    1

--- Stomatology/Main.cs
+++ Stomatology/Main.cs
@@ -22,7 +22,7 @@ namespace Stomatology
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)///
         {
            
 
