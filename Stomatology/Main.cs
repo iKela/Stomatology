6aedfd509a5f81a979c8b9cef50e29f8a523daa9
iKela <kovalchuknm1997@gmail.com>
2017-10-23 10:11:27 +0300

    fgdfgdfgdf

--- Stomatology/Main.cs
+++ Stomatology/Main.cs
@@ -7,19 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Timers;
 
 namespace Stomatology
 {
     public partial class Main : Form
     {
+        System.Timers.Timer timer;
+        int hours, minutes, seconds;
+
         public Main()
         {
             InitializeComponent();
-  
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+           
+
+
+            //Defoult visible of second panel
             panel3.Visible = false;
 
             listView1.View = View.Details;
@@ -37,12 +45,35 @@ namespace Stomatology
 
             //Add items in the listview
             string[] arr = new string[6];
-            ListViewItem itm;
+
+            timer = new System.Timers.Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += OnTimeEvent;
         }
+
+        private void OnTimeEvent(object sender, ElapsedEventArgs e)
+        {
+            Invoke(new Action(() => 
+            {
+                seconds += 1;
+                if (seconds == 60)
+                {
+                    seconds = 0;
+                    minutes += 1;
+                }
+                if (minutes == 60)
+                {
+                    minutes = 0;
+                    hours += 1;
+                }
+                txtResult.Text = string.Format("{0}:{1}:{2}", hours.ToString().PadLeft(2, '0'), minutes.ToString().PadLeft(2, '0'), seconds.ToString().PadLeft(2, '0'));
+            }));
+           
+        }
+
         private void EditAppoinment_Click(object sender, EventArgs e)
         {
-            EditAppoinment newForm = new EditAppoinment();
-            newForm.Show();
+            panel3.Visible = true;
         }
 
         private void AddNewAppoinment_Click(object sender, EventArgs e)
@@ -64,10 +95,27 @@ namespace Stomatology
 
         private void button5_Click(object sender, EventArgs e)
         {
+            timer.Start();
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
+            timer.Stop();
+            txtResult.Text = "00:00:00";
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer.Stop();
+            Application.DoEvents();
         }
     }
 }
