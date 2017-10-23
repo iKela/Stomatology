09f94729f774351cb1fe8ff552bddee662ac82d7
Yuriy Volyanyuk <yzik333@outlook.com>
2017-10-23 12:52:16 +0300

    new patient

--- Stomatology/Patient/NewPatient.cs
+++ Stomatology/Patient/NewPatient.cs
@@ -24,23 +24,9 @@ namespace Stomatology
 
         private void NewPatient_Load(object sender, EventArgs e)
         {
-           // testCon.Open();
-           // SqlCommand cmd = testCon.CreateCommand();
-           // cmd.CommandType = CommandType.Text;
-           // cmd.CommandText = $"INSERT INTO Client (Адрес) values('1')";
-           // cmd.ExecuteNonQuery();
-           // testCon.Close();
-           // //updateTable();
-           // testCon.Open();
-           // //string uId = clientDataGridView.Rows[clientDataGridView.Rows.Count - 1].Cells[0].Value.ToString();
-           //// string query = $"delete from Client where id = {uId}";
-           //// SqlCommand cmd1 = new SqlCommand(query, testCon);
-           // //cmd1.ExecuteNonQuery();
-           // testCon.Close();
-           // //updateTable();
+           
         }
 
-        
 
         private void button2_Click(object sender, EventArgs e)
         {
