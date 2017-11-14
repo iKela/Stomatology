ae76b2af3b1db00400a63d93ff63713c0fd08224
iKela <kovalchuknm1997@gmail.com>
2017-11-14 18:13:34 +0200

    1

--- Stomatology/Properties/AssemblyInfo.cs
+++ Stomatology/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Stomatology")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Dentistry")]
+[assembly: AssemblyDescription("Software for dentistry that replace paperwork.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Stomatology")]
+[assembly: AssemblyCompany("InSoP")]
+[assembly: AssemblyProduct("Dentistry")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
